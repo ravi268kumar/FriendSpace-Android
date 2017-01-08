4f905719a4cc9ac128cbd1b6d39a9620a9c285e6
vishaln04 <vishalnagarale@gmail.com>
2017-01-08 12:24:43 +0530

    FaceBook Login Implemented

--- app/src/main/java/com/example/vishal/friendspace/MainActivity.java
+++ app/src/main/java/com/example/vishal/friendspace/MainActivity.java
@@ -1,13 +1,80 @@
 package com.example.vishal.friendspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
+import android.util.Log;
+import android.widget.Button;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static CallbackManager mFacebookCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mFacebookCallbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_main);
+
+        LoginButton mFacebookSignInButton = (LoginButton)findViewById(R.id.facebook_sign_in_button);
+        mFacebookSignInButton.registerCallback(mFacebookCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(final LoginResult loginResult) {
+                        //TODO: Use the Profile class to get information about the current user.
+
+                        Profile profile = Profile.getCurrentProfile();
+                        Log.d("MainActivity",""+profile);
+                        Log.d("MainActivity",""+profile.getFirstName());
+
+                        handleSignInResult(new Callable<Void>() {
+                            @Override
+                            public Void call() throws Exception {
+                                LoginManager.getInstance().logOut();
+                                return null;
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        handleSignInResult(null);
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Log.d(MainActivity.class.getCanonicalName(), error.getMessage());
+                        handleSignInResult(null);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // (...)
+
+        mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleSignInResult(Object o) {
+        Log.d("MainActivity","handleSignInResult");
     }
 }
