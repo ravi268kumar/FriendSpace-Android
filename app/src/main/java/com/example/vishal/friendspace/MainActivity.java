bfbdcff6acb246b4b759b6f1af781c1178a28301
vishaln04 <vishalnagarale@gmail.com>
2017-01-08 19:25:52 +0530

    Google Login Implemented

--- app/src/main/java/com/example/vishal/friendspace/MainActivity.java
+++ app/src/main/java/com/example/vishal/friendspace/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.facebook.CallbackManager;
@@ -15,19 +16,29 @@ import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity {
 
     public static CallbackManager mFacebookCallbackManager;
+    public static GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Facebook variables
         FacebookSdk.sdkInitialize(getApplicationContext());
         mFacebookCallbackManager = CallbackManager.Factory.create();
+
 
         setContentView(R.layout.activity_main);
 
@@ -63,18 +74,69 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        // Login Button for Google
+
+        SignInButton mGoogleSignInButton = (SignInButton)findViewById(R.id.google_sign_in_button);
+        mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+            }
+        });
     }
 
-    @Override
+   /* @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         // (...)
 
         mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }*/
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()) {
+                //final GoogleApiClient client = mGoogleApiClient;
+                GoogleSignInAccount account = result.getSignInAccount();
+                Log.d("MainActivityGoogle",""+account.getEmail());
+
+                //handleSignInResult(...)
+            } else {
+                //handleSignInResult(...);
+            }
+        } else {
+            // Handle other values for requestCode
+
+            mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+        }
     }
 
     private void handleSignInResult(Object o) {
         Log.d("MainActivity","handleSignInResult");
+    }
+
+    private void signInWithGoogle() {
+        if(mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        final Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 }
